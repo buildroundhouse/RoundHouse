8bc05315d06e252c6e26c51f72aac38ad6ed2b77
buildroundhouse <buildroundhouse@gmail.com>
2026-09-15 06:27:30 -0500

    Mount Business Entity setup routes

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -29,6 +29,7 @@ import companyNoticesRouter from "./companyNotices";
 import gameRoomRouter from "./game-room";
 import presetChipsRouter from "./preset-chips";
 import adminDemoProfilesRouter from "./admin-demo-profiles";
+import entitySetupRouter from "./entity-setup";
 import entitiesRouter from "./entities";
 import conciergeRouter from "./concierge";
 
@@ -67,6 +68,7 @@ router.use(companyNoticesRouter);
 router.use(gameRoomRouter);
 router.use(presetChipsRouter);
 router.use(adminDemoProfilesRouter);
+router.use(entitySetupRouter);
 router.use(entitiesRouter);
 router.use(conciergeRouter);
 
